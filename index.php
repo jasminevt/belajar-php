c2b1c903928901c99b732d70de0f2297ba5e06b6
jasminne fridayanti putri safrida <jasminnefridayanti22@gmail.com>
2022-10-24 20:00:25 +0700

    belajar_php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "hello world <br>";
+$name = "Jasminne Fridayanti Putri Safrida";
+$umur = 20;
+echo "nama saya $name, saya berumur $umur tahun <br>";
+
+$namaadik = "Ukassya";
+$umuradik = 1;
+echo "nama adik saya $namaadik, adik saya berusia $umuradik <br>";
+$selisih = $umur - $umuradik;
+echo "selisih usia saya dan adik saya adalah $selisih tahun";
+?>
+
